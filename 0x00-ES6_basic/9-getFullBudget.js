b52f210b9d5a5addfd64d50d5d9bcc8a02db5cfc
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2024-06-24 23:42:56 +0000

    Add ES6 Basics Task Files

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,11 +1,15 @@
-iimport getBudgetObject from './7-getBudgetObject';
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
+    getIncomeInDollars: function (income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros: function (income) {
+      return `${income} euros`;
+    },
   };
 
   return fullBudget;
