eb85c2ab1c7f7379bf3c365475e71329a312fa29
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2024-06-25 05:00:16 +0000

    Update task 9 & 14 files

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,10 @@
-iexport default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
 }
